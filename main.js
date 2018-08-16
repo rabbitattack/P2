f1a97803fb80aee685a73b39e1146e3d4c5d5716
Jedi77 <jprentup@hotmail.com>
2018-08-16 08:25:08 -0600

    Minor changes cards stagnant

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-
 axios.get('http:www.clashapi.xyz/api/cards')
     .then(function (result) {
         console.log(result)
         createProfileCards(result.data)
-        // for (let i = 0; i < result.data.length; i++) {
-        //     //get the card from the array of results
-        //     var cardObj = result.data[i]
+        flipCard(result.data)
+        for (let i = 0; i < result.data.length; i++) {
+            //get the card from the array of results
+            var cardObj = result.data[i]
 
-        //     //use a selector to select the body
-        //     var body = document.querySelector("body");
+            //use a selector to select the body
+            var body = document.querySelector("body");
 
-        //     //make an img element
-        //     var img = document.createElement("img");
+            //make an img element
+            var img = document.createElement("img");
 
-        //     //set src of that img to the car img
-        //     img.src = `http:www.clashapi.xyz/images/cards/${cardObj.idName}.png`;
+            //set src of that img to the car img
+            img.src = `http:www.clashapi.xyz/images/cards/${cardObj.idName}.png`;
 
-        //     //append that img to body
-        //     body.appendChild(img);
+            //append that img to body
+            body.appendChild(img);
 
-        // }
+        }
 
 
     })
@@ -32,10 +32,11 @@ function createProfileCards(cards) {
     return cards.forEach(card => {
         console.log(card)
         cardContainer.innerHTML += (`
-                <div class="beer-card">
+                <div class="player-card">
                     <div class="front">
                         <h2>${card.idName}</h2>
                         <img src=${`http:www.clashapi.xyz/images/cards/${card.idName}.png`}></img>
+                        <p>${card.rarity}</p>
                         <p>${card.elixirCost}</p>
                         <p>${card.type}</p>
                     </div>
@@ -46,12 +47,12 @@ function createProfileCards(cards) {
     })
 }
 
-// function flipCard() {
-//     const createProfileCard = document.getElementsByTagName('img')
-//     beerCards.forEach(beer => {
-//         beer.addEventListener('click', (() => {
-//             beer.classList.toggle('clicked')
-//             beer.firstElementChild.nextElementSibling.classList.toggle('flipped')
-//         }))
-//     })
-// }
+function flipCard(warrior) {
+    const createProfileCard = document.getElementsByTagName('img')
+    flipCard.forEach(warrior => {
+        result.data.addEventListener('click', (() => {
+            result.data.classList.toggle('clicked')
+            result.data.firstElementChild.nextElementSibling.classList.toggle('flipped')
+        }))
+    })
+}
